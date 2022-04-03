4c0b8f55546bd31878fb9518c1343304e86ad2ff
Khoa dep trai <khoa.np1195@sinhvien.hoasen.edu.vn>
2022-04-03 18:31:15 +0700

    Get Google Map

---
+++ bando/MainActivity.cs
@@ -0,0 +1,145 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+using Android.Views;
+using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
+using DotLiquid.Tags;
+using Android.Support.V4.App;
+using Android;
+using Android.Content.PM;
+
+namespace bando
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/UberTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, IOnMapReadyCallback
+    {
+       
+        Android.Support.V7.Widget.Toolbar mainToolbar;
+        Android.Support.V4.Widget.DrawerLayout drawerLayout;
+        GoogleMap mainMap;
+        readonly string[] permissionGroupLocation = { Manifest.Permission.AccessCoarseLocation, Manifest.Permission.AccessFineLocation };
+        const int requestLocationId = 0;
+        //public static MainActivity newInstance()
+        //{
+        //    MainActivity fragment = new MainActivity();
+        //    return fragment;
+        //}
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+           base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            var mapFragment = (MapFragment)FragmentManager.FindFragmentById(Resource.Id.map);
+            mapFragment.GetMapAsync(this);
+
+            CheckLocationPermission();
+            //  SupportMapFragment mapFragment = SupportFragmentManager.FindFragmentById(Resource.Id.map).JavaCast<SupportMapFragment>();
+            // SupportMapFragment mapFragment = (SupportMapFragment)SupportFragmentManager.FindFragmentById(Resource.Id.map);
+
+            //SupportMapFragment mapFragment = SupportFragmentManager.FindFragmentById(Resource.Id.map).JavaCast<SupportMapFragment>()
+            // ;
+            //SupportMapFragment.NewInstance(mainMap);
+            //mapFragment.GetMapAsync(this);
+            //     mapFragment.GetMapAsync(this);
+
+            // SetUpMap();
+
+            ConnectControl();
+        }
+
+
+        //private void SetUpMap()
+        //{
+        //    if(mainMap == null)
+        //    {
+        //        //FragmentManager.FindFragmentById<MapFragment>(Resource.Id.map).GetMapAsync(this);
+        //        FragmentManager.FindFragmentById<MapFragment>(Resource.Id.map).GetMapAsync(this);
+        //    }
+        //}
+
+
+        void ConnectControl()
+        {
+            drawerLayout = (Android.Support.V4.Widget.DrawerLayout)FindViewById(Resource.Id.drawerLayout);
+            mainToolbar = (Android.Support.V7.Widget.Toolbar)FindViewById(Resource.Id.mainToolbar);
+            SetSupportActionBar(mainToolbar);
+            SupportActionBar.Title = "";
+            Android.Support.V7.App.ActionBar actionBar = SupportActionBar;
+            actionBar.SetHomeAsUpIndicator(Resource.Mipmap.ic_menu_action);
+            actionBar.SetDisplayHomeAsUpEnabled(true);
+
+        }
+
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Android.Resource.Id.Home:
+                    drawerLayout.OpenDrawer((int)GravityFlags.Left);
+                    return true;
+
+                default:
+                    return base.OnOptionsItemSelected(item);
+
+
+            }
+        }
+
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            mainMap = googleMap;
+        }
+
+        //public void OnMapReady(GoogleMapOptions googleMapOptions)
+        //{
+        //    mainMap = googleMapOptions;
+
+
+
+        //    // Add a marker in Sydney and move the camera
+        //    //LatLng sydney = new LatLng(-34, 151);
+        //    //mainMap.AddMarker(new MarkerOptions().SetPosition(sydney).SetTitle("Marker in Sydney"));
+        //    //mainMap.MoveCamera(CameraUpdateFactory.NewLatLng(sydney));
+        //}
+
+        bool CheckLocationPermission()
+        {
+            bool permissionGranted = false;
+
+            if (ActivityCompat.CheckSelfPermission(this, Manifest.Permission.AccessFineLocation) != Android.Content.PM.Permission.Granted
+                && ActivityCompat.CheckSelfPermission(this, Manifest.Permission.AccessCoarseLocation) != Android.Content.PM.Permission.Granted)
+            {
+                permissionGranted = false;
+                RequestPermissions(permissionGroupLocation, requestLocationId);
+
+            }
+            else
+                {
+                permissionGranted = true;
+
+            }
+            return permissionGranted;
+
+
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+           if(grantResults[0] == (int)Android.Content.PM.Permission.Granted)
+            {
+                Toast.MakeText(this, "Permission was granted", ToastLength.Short).Show();
+            }
+            else
+            {
+                Toast.MakeText(this, "Permission was dennied", ToastLength.Short).Show();
+            }
+        }
+
+
+    }
+}
---
+++ bando/obj/Debug/110/android/src/crc6420f6e58f2c675d7a/MainActivity.java
@@ -0,0 +1,83 @@
+package crc6420f6e58f2c675d7a;
+
+
+public class MainActivity
+	extends androidx.appcompat.app.AppCompatActivity
+	implements
+		mono.android.IGCUserPeer,
+		com.google.android.gms.maps.OnMapReadyCallback
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"n_onOptionsItemSelected:(Landroid/view/MenuItem;)Z:GetOnOptionsItemSelected_Landroid_view_MenuItem_Handler\n" +
+			"n_onRequestPermissionsResult:(I[Ljava/lang/String;[I)V:GetOnRequestPermissionsResult_IarrayLjava_lang_String_arrayIHandler\n" +
+			"n_onMapReady:(Lcom/google/android/gms/maps/GoogleMap;)V:GetOnMapReady_Lcom_google_android_gms_maps_GoogleMap_Handler:Android.Gms.Maps.IOnMapReadyCallbackInvoker, Xamarin.GooglePlayServices.Maps\n" +
+			"";
+		mono.android.Runtime.register ("bando.MainActivity, bando", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("bando.MainActivity, bando", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("bando.MainActivity, bando", "System.Int32, mscorlib", this, new java.lang.Object[] { p0 });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+
+	public boolean onOptionsItemSelected (android.view.MenuItem p0)
+	{
+		return n_onOptionsItemSelected (p0);
+	}
+
+	private native boolean n_onOptionsItemSelected (android.view.MenuItem p0);
+
+
+	public void onRequestPermissionsResult (int p0, java.lang.String[] p1, int[] p2)
+	{
+		n_onRequestPermissionsResult (p0, p1, p2);
+	}
+
+	private native void n_onRequestPermissionsResult (int p0, java.lang.String[] p1, int[] p2);
+
+
+	public void onMapReady (com.google.android.gms.maps.GoogleMap p0)
+	{
+		n_onMapReady (p0);
+	}
+
+	private native void n_onMapReady (com.google.android.gms.maps.GoogleMap p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
